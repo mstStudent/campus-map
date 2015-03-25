a9bf754f93c092afbd39fb6d4ba7a586dabe2786
mststudent <jpty9@mst.edu>
2015-03-25 15:09:55 -0500

    basic map display, bootstrap top menu, and drawning tool added.

---
+++ js/map.js
@@ -0,0 +1,60 @@
+var campus_map = L.map('map').setView([37.95451, -91.77386], 17);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://cloudmade.com">CloudMade</a>',
+    subdomain: ['a','b','c'],
+    maxZoom: 25,
+}).addTo(campus_map);
+
+
+
+// Initialise the FeatureGroup to store editable layers
+var drawnItems = new L.FeatureGroup();
+campus_map.addLayer(drawnItems)
+
+// Initialise the draw control and pass it the FeatureGroup of editable layers
+var drawControl = new L.Control.Draw({
+    draw: {
+        circle: false
+    },
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+campus_map.addControl(drawControl);
+
+campus_map.on('draw:created', function (e) {
+        var popup = L.popup();
+        var type = e.layerType,
+                layer = e.layer;
+        drawnItems.addLayer(layer);
+        var content = "";
+        layer._latlngs.forEach(function(value){
+                content = content + value['lat']+' , ' + value['lng'] + '<br><br>';
+        });
+        var update = confirm("Already Know the Room Number/name?");
+                if(update){
+                        layer.roomName = prompt("Name",'Room #');
+                }
+        popup.setContent(String(layer.roomName)+'<br>'+content)
+
+        layer.bindPopup(popup).openPopup();
+
+        layer.on('click',function(){
+                var popup = L.popup();
+                var content = "";
+                layer._latlngs.forEach(function(value){
+                        content = content + value['lat']+' , ' + value['lng'] + '<br><br>';
+                });
+                var update = confirm("Update the Room Number/name?");
+                        if(update){
+                                layer.roomName = prompt("Name",'Room #');
+                        }
+                popup.setContent(String(layer.roomName)+'<br>'+content)
+
+                layer.bindPopup(popup).openPopup();
+        });
+});
+
+L.control.mousePosition().addTo(campus_map);
+
