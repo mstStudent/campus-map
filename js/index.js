29231a27385f98fcb150dd98c312bf2c7674eb8e
mststudent <jpty9@mst.edu>
2015-04-07 08:55:17 -0500

    Dummy code to insert data to database, if it doesn't work guess we can do it manually until someone can correct this code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,50 @@
-$()
+$("#addRoom").click(function(){
+  addRoom();
+});
+
+$("#addBuilding").click(function(){
+  addBuilding();
+});
 
 function addRoom(){
+  var bName = $('#buildingName_forRooms').val();
+  var floor = $('#floorSelect').val();
+  console.log('bName: ', bName);
+  console.log('floor: ', floor);
+ 
+  $.each(drawnItems._layers ,function(index,room){
+      if(room.roomName){
+        console.log('Room Name: ', room.roomName);
+      }
+      else{
+        console.log('Room Name is needed');
+      }
+      console.log('room: ', room._latlngs);
+  })
+
+  
+
+}
+
+function addBuilding(){
+  var bName = $('#buildingName').val();
+  var numFloors = $('#numFloorsSelect').val();
+  var mainFloor = $('#mainFloorSelect').val();
+
+  console.log('bName: ', bName);
+  console.log('numFloors: ', numFloors);
+  console.log('defFloor: ', mainFloor);
+
+  var request = new XMLHttpRequest();
+
+  $.ajax({
+    url: "/php/temp_add_building.php",
+    type: "POST",
+    data: ({bname: bName, fCount: numFloors, defFloor: mainFloor}),
+    dataType: 'json',
+    success: function(test){
+	console.log("YES: ", test)
+    }
+  });
+
 }
