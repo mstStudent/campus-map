d20abc1efbcbca6911d48ccf1c4d270ed13187cd
mststudent <jpty9@mst.edu>
2015-04-20 18:52:56 -0500

    get all latlng of all buildings so far, now we need to use leaflet.indoor

--- php/getAllBuildingCoords.php
+++ php/getAllBuildingCoords.php
@@ -3,8 +3,8 @@
   require("basicFunctions.php");
 
    
-#  $buildingName = $_GET['bName'];
-$query = "select latitude, longitude from coordinates where buildingID in "
+  $buildingName = $_GET['bName'];
+  $query = "select latitude, longitude from coordinates where buildingID in "
 			. "( select id from buildings where name = '" . $buildingName . "' );";
   echo queryDB($query);
 ?>
