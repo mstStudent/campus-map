a7f50bd514929e373597ade1d815182b52100ebf
mststudent <jpty9@mst.edu>
2015-04-20 18:41:54 -0500

    redo php

---
+++ php/getAllBuildingCoords.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+  require("basicFunctions.php");
+
+   
+#  $buildingName = $_GET['bName'];
+$query = "select latitude, longitude from coordinates where buildingID in "
+			. "( select id from buildings where name = '" . $buildingName . "' );";
+  echo queryDB($query);
+?>
